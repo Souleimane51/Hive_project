123ba7c44949a324ba2e9afbf055989efb0c432e
Souleimane Amiche <souleimane51000@gmail.com>
2024-04-26 00:08:36 +0100

    Continuing the programing of the hive project

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const hamburgerMenu = document.querySelector(".hamburgerMenu");
-const sideNav = document.querySelector(".tabsSubContainer")
+const sideNav = document.querySelector(".tabsSubContainer");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const bg = document.querySelector(".bg");
+const nav = document.querySelector("nav");
+const images = document.querySelectorAll(".gallery img");
+const categories = document.querySelectorAll(".cardContainer");
+const body = document.querySelector("body");
 let touchStart;
 let touchEnd;
+let currentPosition;
+let oldPosition;
+
 
 hamburgerMenu.addEventListener("click", () => {
     hamburgerMenu.classList.toggle("active")
     sideNav.classList.toggle("active")
-    document.querySelector("body").classList.toggle("active")
+    body.classList.toggle("active")
 })
 
 // Close sideNav with swiping
@@ -22,7 +32,73 @@ sideNav.addEventListener('touchend', () => {
     if (touchStart - touchEnd > 100) {
         sideNav.classList.remove('active');
         hamburgerMenu.classList.remove('active');
-        document.querySelector("body").classList.remove("active")
+        body.classList.remove("active")
 
     }
 });
+
+window.addEventListener("scroll", () => {
+    currentPosition = window.scrollY;
+    if (currentPosition <= 5 || currentPosition < oldPosition) {
+        nav.style.transform="translateY(0%)";
+    }
+    else {
+        nav.style.transform="translateY(-100%)";
+    }
+
+    oldPosition = currentPosition <= 0 ? 0 : currentPosition;
+})
+
+
+images.forEach(img => {
+
+    img.addEventListener("click", () => {
+
+        bg.classList.add("active");
+        currentId = img.dataset.index;
+
+        let newImg = document.createElement('img');
+        newImg.src = img.attributes.src.value;
+        newImg.alt = "Image of the hive group"
+        newImg.classList.add("fullSizeImage")
+        bg.append(newImg)
+
+        nav.style.transform="translateY(-100%)";
+        body.style.overflowY = "hidden";
+
+    })
+})
+
+bg && bg.addEventListener("click", (e) => {
+
+    if (e.target.classList.contains('bg') || e.target.classList.contains('close')) {
+        bg.classList.remove('active');
+        document.querySelector(".fullSizeImage").remove();
+        body.style.overflowY = "scroll";
+
+        if (window.scrollY === 0) {
+            nav.style.transform="translateY(0%)";
+        }
+    }
+
+})
+
+categories.forEach(category => {
+
+    category.addEventListener("click", () => {
+
+        images.forEach(img => {
+            if (!img.dataset.index.includes(category.dataset.index)) {
+                img.style.display = "none";
+                console.log("if ==>", img.dataset.index);
+                console.log("if ==>", category.dataset.index);
+            }
+            else {
+                img.style.display = "block";
+                console.log("else ==>", img.dataset.index);
+                console.log("else ==>", category.dataset.index);
+            }
+        })
+
+    })
+})
